bd0138079b99fe90402a9182b45394a4ab91a4fe
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2023-10-20 11:55:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import Footer from "./components/Footer";
